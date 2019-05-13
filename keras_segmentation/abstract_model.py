0d736b386c5aad3b0c61563881316957eb833dbc
Chirag Karia <trichiragkaria@gmail.com>
2019-05-13 13:13:41 -0400

    Add load_weights and predict method

--- keras_segmentation/abstract_model.py
+++ keras_segmentation/abstract_model.py
@@ -1,10 +1,19 @@
 import os
 import glob
 import warnings
+import random
 
 import keras
+import cv2
+import numpy as np
+
 from .models import model_from_name
-from.data_utils.data_loader import image_segmentation_generator, verify_segmentation_dataset
+from .models.config import IMAGE_ORDERING
+from.data_utils.data_loader import image_segmentation_generator, verify_segmentation_dataset, get_image_arr
+
+
+random.seed(0)
+class_colours = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in range(5000)]
 
 
 class ModelBase:
@@ -89,30 +98,51 @@ class ModelBase:
             raise AssertionError("Please increase epochs value, training is starting at epoch {} and total number of"
                                  " epochs specified for training is {}".format(initial_epoch, epochs))
 
-        if validate:
-            history = self.model.fit_generator(
-                generator=train_gen,
-                steps_per_epoch=steps_per_epoch,
-                epochs=epochs,
-                verbose=1,
-                callbacks=callbacks,
-                validation_data=val_gen,
-                validation_steps=100,
-                shuffle=True,
-                use_multiprocessing=True,
-                initial_epoch=initial_epoch
-            )
-        else:
-            history = self.model.fit_generator(
-                generator=train_gen,
-                steps_per_epoch=steps_per_epoch,
-                epochs=epochs,
-                verbose=1,
-                callbacks=callbacks,
-                shuffle=True,
-                use_multiprocessing=True,
-                initial_epoch=initial_epoch
-            )
+        history = self.model.fit_generator(
+            generator=train_gen,
+            steps_per_epoch=steps_per_epoch,
+            epochs=epochs,
+            verbose=1,
+            callbacks=callbacks,
+            validation_data=val_gen if validate else None,
+            validation_steps=100 if validate else None,
+            shuffle=True,
+            use_multiprocessing=True,
+            initial_epoch=initial_epoch
+        )
 
         self.curr_epoch = epochs
         return history
+
+    def load_weights(self, saved_weights_path):
+        self.curr_epoch = int(saved_weights_path.split("_")[-1].split(".")[0])
+        self.model.load_weights(saved_weights_path)
+
+    def predict(self, input_img, out_fname=None):
+        assert (type(input_img) == str) or (type(input_img) == np.ndarray), "Input should be the ndarray of an image" \
+                                                                            " or a filename."
+
+        if type(input_img) == str:
+            input_img = cv2.imread(input_img)
+
+        x = get_image_arr(input_img, self.model.input_width, self.model.input_height, odering=IMAGE_ORDERING)
+
+        prediction = self.model.predict(np.array([x]))[0]
+        prediction = prediction.reshape(
+            (self.model.output_height, self.model.output_width, self.model.n_classes)
+        ).argmax(axis=2)
+
+        seg_img = np.zeros((self.model.output_height, self.model.output_width, 3))
+        colours = class_colours
+
+        for c in range(self.model.n_classes):
+            seg_img[:, :, 0] += ((prediction[:, :] == c) * (colours[c][0])).astype('uint8')
+            seg_img[:, :, 1] += ((prediction[:, :] == c) * (colours[c][1])).astype('uint8')
+            seg_img[:, :, 2] += ((prediction[:, :] == c) * (colours[c][2])).astype('uint8')
+
+        seg_img = cv2.resize(seg_img, (self.model.input_width, self.model.input_height))
+
+        if out_fname is not None:
+            cv2.imwrite(out_fname, seg_img)
+
+        return prediction
